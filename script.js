38a7cf900f5680bb9da7d343f06a8918012e0725
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-20 18:28:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const div1 = document.querySelector("#square1")
+const div2 = document.querySelector("#square2")
+const div3 = document.querySelector("#square3")
+const body = document.querySelector("body")
+// const div = document.querySelectorAll("div")
+// div1.style.backgroundColor = "#E6E6FA"
+// div2.style.backgroundColor = "#E6E6FA"
+// div3.style.backgroundColor = "#E6E6FA"
+div1.addEventListener("mouseover", ()=>{
+	div2.style.backgroundColor = "#6F4E37"
+	div3.style.backgroundColor = "#6F4E37"
+	div1.style.backgroundColor = "#E6E6FA"
+})   
+div2.addEventListener("mouseover", ()=>{
+	div1.style.backgroundColor = "#6F4E37"
+	div3.style.backgroundColor = "#6F4E37"
+	div2.style.backgroundColor = "#E6E6FA"
+})
+div3.addEventListener("mouseover", ()=>{
+	div1.style.backgroundColor = "#6F4E37"
+	div2.style.backgroundColor = "#6F4E37"
+	div3.style.backgroundColor = "#E6E6FA"
+})
+body.addEventListener("mouseout", ()=>{
+	div1.style.backgroundColor = "#E6E6FA"
+	div2.style.backgroundColor = "#E6E6FA"
+	div3.style.backgroundColor = "#E6E6FA"
+})
